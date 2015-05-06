42409b363d98bb0744f0afc7ffc497d10cf28adf
Edvandro <ealves32@hotmail.com.br>
2015-05-06 19:07:38 -0300

    Update graficos.h
    icone

--- graficos.h
+++ graficos.h
@@ -11,6 +11,7 @@
 void nome_console();/*função para mudar o nome do console usado a biblioteca console*/
 void maximiza_console();/*função para maximizar a janela do console usado a biblioteca console*/
 void velas();
+void icone();
 
 
 #endif /*_GRAFICOS_*/
